ee569b5d18ab343bfa7653f18d777c482cee96dc
ATravelingWizard <henardgertzen@gmail.com>
2023-08-31 14:07:33 +0200

    added main database constr

--- Skreenkinikor_Master_Project/Classes/ConnectionStrings.cs
+++ Skreenkinikor_Master_Project/Classes/ConnectionStrings.cs
@@ -9,5 +9,6 @@ namespace Skreenkinikor_Master_Project.Classes
     public static class ConnectionStrings
     {
         public const string conLoginStr = "Data Source=.;Initial Catalog=LoginDB;Integrated Security=True";
+        public const string conSkreenMainStr = "Data Source=.;Initial Catalog = ScreenKinikor; Integrated Security = True";
     }
 }
