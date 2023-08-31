5ab2f860bd85ade36cb41c05d95c5bd491138114
ATravelingWizard <henardgertzen@gmail.com>
2023-08-31 14:17:29 +0200

    changed form borders to none

--- Skreenkinikor_Master_Project/Child Forms/frmActors.Designer.cs
+++ Skreenkinikor_Master_Project/Child Forms/frmActors.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmActors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmActors";
             this.Text = "frmActors";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.Designer.cs
+++ Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmCon_Sale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmCon_Sale";
             this.Text = "frmCon_Sale";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
