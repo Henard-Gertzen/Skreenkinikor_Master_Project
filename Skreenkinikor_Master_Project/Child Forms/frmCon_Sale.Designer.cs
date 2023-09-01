afdb2ebc019aec229bd9a82c5237b05ccc22add6
Stefan Janse van Rensburg <33706956@student.g.nwu.ac.za>
2023-09-01 14:24:08 +0200

    Test

--- Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.Designer.cs
+++ Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.Designer.cs
@@ -28,20 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvConItems = new System.Windows.Forms.DataGridView();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConItems)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
+            this.panel1.Controls.Add(this.btnProceed);
+            this.panel1.Controls.Add(this.dgvConItems);
+            this.panel1.Controls.Add(this.txtSearch);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(638, 297);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtSearch.Location = new System.Drawing.Point(429, 0);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(209, 20);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // dgvConItems
+            // 
+            this.dgvConItems.AllowUserToAddRows = false;
+            this.dgvConItems.AllowUserToDeleteRows = false;
+            this.dgvConItems.AllowUserToResizeColumns = false;
+            this.dgvConItems.AllowUserToResizeRows = false;
+            this.dgvConItems.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvConItems.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(67)))), ((int)(((byte)(90)))));
+            this.dgvConItems.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvConItems.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dgvConItems.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(67)))), ((int)(((byte)(90)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(241)))), ((int)(((byte)(242)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(67)))), ((int)(((byte)(90)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvConItems.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvConItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(67)))), ((int)(((byte)(90)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(194)))), ((int)(((byte)(231)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvConItems.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgvConItems.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dgvConItems.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvConItems.EnableHeadersVisualStyles = false;
+            this.dgvConItems.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(241)))), ((int)(((byte)(242)))));
+            this.dgvConItems.Location = new System.Drawing.Point(0, 0);
+            this.dgvConItems.Name = "dgvConItems";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(194)))), ((int)(((byte)(231)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvConItems.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dgvConItems.RowHeadersVisible = false;
+            this.dgvConItems.RowTemplate.Height = 35;
+            this.dgvConItems.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvConItems.Size = new System.Drawing.Size(414, 297);
+            this.dgvConItems.TabIndex = 6;
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.Location = new System.Drawing.Point(459, 41);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(179, 34);
+            this.btnProceed.TabIndex = 7;
+            this.btnProceed.Text = "button1";
+            this.btnProceed.UseVisualStyleBackColor = true;
+            this.btnProceed.Click += new System.EventHandler(this.btnProceed_Click);
             // 
             // frmCon_Sale
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(41)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(647, 309);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmCon_Sale";
             this.Text = "frmCon_Sale";
+            this.Load += new System.EventHandler(this.frmCon_Sale_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConItems)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvConItems;
+        private System.Windows.Forms.Button btnProceed;
     }
 }
--- Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.cs
+++ Skreenkinikor_Master_Project/Child Forms/frmCon_Sale.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Skreenkinikor_Master_Project.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Skreenkinikor_Master_Project
 {
@@ -15,6 +18,98 @@ namespace Skreenkinikor_Master_Project
         public frmCon_Sale()
         {
             InitializeComponent();
+        }
+
+        private string connectionString = ConnectionStrings.conSkreenMainStr;
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmCon_Sale_Load(object sender, EventArgs e)
+        {
+            // loads the the confectionary items into the datagrid when the form loads
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    
+                    string sql = "SELECT Confectionary_Name, Confectionary_Price FROM Confectionary_Item Where Confectionary_Stock > 0";
+                    using (SqlCommand command = new SqlCommand(sql,conn))
+                    {
+                        using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                        {
+                            using (DataSet ds = new DataSet())
+                            {
+
+
+                                dataAdapter.SelectCommand = command;
+                                dataAdapter.Fill(ds, "Confectionary_Item");
+
+                               
+
+                                dgvConItems.DataSource = ds;
+                                dgvConItems.DataMember = "Confectionary_Item";
+
+                                dgvConItems.Columns["Confectionary_Name"].HeaderText = "Item";
+                                dgvConItems.Columns["Confectionary_Price"].HeaderText = "Price";
+                            }
+                        }
+                    }
+                }
+                catch (SqlException error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+                
+            
+        }
+
+        private void btnProceed_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                    {
+                        using (DataSet ds = new DataSet())
+                        {
+                            // the text in the textbox filters the datagrid according to what is typed
+                            string sql = "SELECT Confectionary_Name, Confectionary_Price FROM Confectionary_Item Where Confectionary_Stock > 0 AND Confectionary_Name COLLATE Latin1_General_CI_AS LIKE '%" + txtSearch.Text + "%'";
+
+                            using (SqlCommand command = new SqlCommand(sql,conn))
+                            {
+                                dataAdapter.SelectCommand= command;
+                                dataAdapter.Fill(ds, "Confectionary_Item");
+
+                                dgvConItems.DataSource= ds;
+                                dgvConItems.DataMember = "Confectionary_Item";
+                            }
+                        }
+                    }
+                }
+                catch(SqlException error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
         }
     }
 }
