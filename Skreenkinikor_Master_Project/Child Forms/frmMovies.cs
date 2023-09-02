5c9cd6ea3e1dd07f354e8248b7b97b5dec6bf195
Stefan Janse van Rensburg <33706956@student.g.nwu.ac.za>
2023-09-02 19:51:35 +0200

    edit button code finalized

--- Skreenkinikor_Master_Project/Child Forms/frmMovies.cs
+++ Skreenkinikor_Master_Project/Child Forms/frmMovies.cs
@@ -23,7 +23,7 @@ namespace Skreenkinikor_Master_Project
         private string Name, Description;
         private decimal Price;
         private string delMovie;
-        private string MovieName, MovieDesc;
+        private string MovieName, MovieDesc,tempName;
         private decimal MoviePrice;
 
         
@@ -121,10 +121,11 @@ namespace Skreenkinikor_Master_Project
 
         private void dgvMovies_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            // adds the values of the selected datagrid into variables
+           // adds the values of the selected datagrid into variables
             delMovie = dgvMovies.Rows[e.RowIndex].Cells["Movie_Name"].Value.ToString();
             MovieName = dgvMovies.Rows[e.RowIndex].Cells["Movie_Name"].Value.ToString();
             MovieDesc = dgvMovies.Rows[e.RowIndex].Cells["Movie_Description"].Value.ToString();
+            tempName = dgvMovies.Rows[e.RowIndex].Cells["Movie_Name"].Value.ToString();
             MoviePrice = (decimal)dgvMovies.Rows[e.RowIndex].Cells["Seat_Price"].Value;
 
             // the textboxes are populated with the values
@@ -171,14 +172,50 @@ namespace Skreenkinikor_Master_Project
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            // makes sure a movie is selected
             if (MovieName == "")
             {
                 MessageBox.Show("Please select a movie to delete");
                 return;
             }
-
             
+            //SQL injection to update the database
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+
+                    MovieName = txtMovieName.Text;
+                    MovieDesc = txtMovieDesc.Text;
+                    MoviePrice = decimal.Parse(txtMoviePrice.Text);
+
+                    conn.Open();
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                    {
+                        string sql = "UPDATE Movie_Info SET Movie_Name = @Name, Movie_Description = @Desc, Seat_Price = @Price WHERE Movie_Name = @Temp";
+                        using (SqlCommand command = new SqlCommand(sql, conn))
+                        {
+                            // adds parameters to the command                                                                      
+                            command.Parameters.AddWithValue("@Name", MovieName);
+                            command.Parameters.AddWithValue("@Desc", MovieDesc);
+                            command.Parameters.AddWithValue("@Price", MoviePrice);
+                            command.Parameters.AddWithValue("@Temp", tempName);
+                            command.ExecuteNonQuery();                       
+                                                      
+                        }
+                    }                   
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                displayDB();
+                MessageBox.Show("Movie updated");
+               
+
+            }
 
         }
     }
